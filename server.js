3732d3c99a0a160f5e0a755ab85fb204b6e77f83
Christopher Clark <chrisdclark3@gmail.com>
2015-08-19 04:21:45 -0700

    cleaned up server.js a bit

--- server.js
+++ server.js
@@ -3,12 +3,4 @@ var app = express();
 var port = process.env.PORT || 6789;
 var server = require('http').createServer(app);
     server.listen(port);
-var fs = require('fs');
-var path = require("path");
-var directory = path.resolve('./');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 app.use(express.static('./app'));
